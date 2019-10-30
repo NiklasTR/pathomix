b00e3c6b531f85a2d87b0b929c34ba576c3757e8
Ubuntu <ubuntu@ip-172-31-32-54.eu-central-1.compute.internal>
2019-10-30 22:08:54 +0000

    downloading preprocessed msigi data

---
+++ data/msi_gi_ffpe_cleaned/download_msi_gi_ffpe_cleaned.sh
@@ -0,0 +1,10 @@
+# downloading pre-tesselated and QCed data from https://zenodo.org/record/1214456#.Xbn63EVKg5k
+wget -v https://zenodo.org/record/2530835/files/CRC_DX_TEST_MSIMUT.zip?download=1
+wget -v https://zenodo.org/record/2530835/files/CRC_DX_TEST_MSS.zip?download=1
+wget -v https://zenodo.org/record/2530835/files/CRC_DX_TRAIN_MSIMUT.zip?download=1
+wget -v https://zenodo.org/record/2530835/files/CRC_DX_TRAIN_MSS.zip?download=1
+wget -v https://zenodo.org/record/2530835/files/STAD_TEST_MSIMUT.zip?download=1
+wget -v https://zenodo.org/record/2530835/files/STAD_TEST_MSS.zip?download=1
+wget -v https://zenodo.org/record/2530835/files/STAD_TRAIN_MSIMUT.zip?download=1
+wget -v https://zenodo.org/record/2530835/files/STAD_TRAIN_MSS.zip?download=1
+unzip '*.zip*'
