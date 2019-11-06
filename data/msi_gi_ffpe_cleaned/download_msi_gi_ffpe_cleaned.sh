8f37f2a5ddd370106b1909e1bfc88bc46aabb4ad
Ubuntu <ubuntu@ip-172-31-32-54.eu-central-1.compute.internal>
2019-11-06 23:24:22 +0000

    download

--- data/msi_gi_ffpe_cleaned/download_msi_gi_ffpe_cleaned.sh
+++ data/msi_gi_ffpe_cleaned/download_msi_gi_ffpe_cleaned.sh
@@ -7,4 +7,26 @@ wget -v https://zenodo.org/record/2530835/files/STAD_TEST_MSIMUT.zip?download=1
 wget -v https://zenodo.org/record/2530835/files/STAD_TEST_MSS.zip?download=1
 wget -v https://zenodo.org/record/2530835/files/STAD_TRAIN_MSIMUT.zip?download=1
 wget -v https://zenodo.org/record/2530835/files/STAD_TRAIN_MSS.zip?download=1
-unzip '*.zip*'
+
+# creating dirs
+mkdir STAD
+mkdir CRC_DX
+mkdir STAD/TEST STAD/TRAIN
+mkdir CRC_DX/TEST CRC_DX/TRAIN
+
+# moving files
+rsync --remove-source-files -rav STAD_TEST* STAD/TEST
+rsync --remove-source-files -rav STAD_TRAIN* STAD/TRAIN
+rsync --remove-source-files -rav CRC_DX_TRAIN* CRC_DX/TRAIN
+rsync --remove-source-files -rav CRC_DX_TEST* CRC_DX/TEST
+
+# unzipping
+unzip 'STAD/TEST/*.zip*' -d STAD/TEST
+unzip 'STAD/TRAIN/*.zip*' -d STAD/TRAIN
+unzip 'CRC_DX/TEST/*.zip*' -d CRC_DX/TEST
+unzip 'CRC_DX/TRAIN/*.zip*' -d CRC_DX/TRAIN
+
+# cleaning zip
+find . -type f -name '*.zip*' -delete
+
+# the data is now distributed and can be used for training using common data generators
