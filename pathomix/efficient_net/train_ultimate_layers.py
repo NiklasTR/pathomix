9d53722910d6432afc3386863381a67175c27087
PeterMFull <p.full@dkfz.de>
2019-11-07 03:19:42 +0100

    effnet added

---
+++ pathomix/efficient_net/train_ultimate_layers.py
@@ -0,0 +1,59 @@
+from keras.layers import Dense
+from keras.models import Model
+import os
+
+from keras import optimizers
+
+
+def train_ultimate_layers(model,
+                          train_generator,
+                          validation_generator,
+                          steps_per_epoch_train,
+                          steps_per_epoch_val,
+                          out_path,
+                          epochs=20,
+                          num_of_dense_layers=0,
+                          dense_layer_dim=32,
+                          tensor_board_callback=None,
+                          bsave=False):
+        # remove final layer
+        # this will remove final classification layer
+        # new last layer has shape (after GlobalAveragepooling2d) (None, 1280)
+        model.layers.pop()
+
+        # freeze all layers in pretrained model
+        for l in model.layers:
+            l.trainable = False
+
+        # add 2 fully connected layers
+        # on toy data the additional Dense(32) layer improved the accuracy for the validation set after the same number of epochs from
+        # 0.9571 to 0.9831. No over fitting was observed
+        x = model.output
+        for dl in range(num_of_dense_layers):
+                x = Dense(dense_layer_dim)(x)
+        pred = Dense(1, activation='sigmoid')(x)
+
+        mymodel = Model(inputs=model.input, outputs=pred)
+
+        # sgd = optimizers.SGD(learning_rate=0.01, decay=1e-6, momentum=0.0)
+        mymodel.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+        #mymodel.compile(optimizer=sgd, loss='binary_crossentropy', metrics=['accuracy'])
+
+        mymodel.fit_generator(
+                train_generator,
+                steps_per_epoch=steps_per_epoch_train,
+                epochs=epochs,
+                validation_data=validation_generator,
+                validation_steps=steps_per_epoch_val,
+                callbacks=[tensor_board_callback])
+
+        if bsave:
+                # serialize model to JSON
+                model_json = mymodel.to_json()
+                with open(os.path.join("{}.json".format(out_path)), "w") as json_file:
+                        json_file.write(model_json)
+                # save weights
+                mymodel.save_weights(os.path.join("{}.h5".format(out_path)))
+                print('model saved at {}'.format(out_path))
+
+        return mymodel
--- pathomix/onnx_to_tf.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-with tf.gfile.FastGFile('resnext50.pb', "rb") as f:
-    graph_def = tf.GraphDef()
-    graph_def.ParseFromString(f.read())
-    g_in = tf.import_graph_def(graph_def, name="")
-
-sess = tf.Session(graph=g_in)
-
